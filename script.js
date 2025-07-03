491707c89091fe783cd6922422dfa827ce1db482
Edwin Vivas <jhovannyvivas@gmail.com>
2025-07-03 00:08:37 -0500

    simplificando el código

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-let numero_instrucciones = 1;
-const ids_botones = ['uno', 'dos', 'tres', 'cuatro'];
+let longitud_secuencia = 1;
 
-const boton_uno = document.getElementById(ids_botones[0])
-const boton_dos = document.getElementById(ids_botones[1])
-const boton_tres = document.getElementById(ids_botones[2])
-const boton_cuatro = document.getElementById(ids_botones[3])
+const cuadro_sup_izq = document.getElementsByClassName("cuadro-sup-izq")[0];
+const cuadro_sup_der = document.getElementsByClassName("cuadro-sup-der")[0];
+const cuadro_inf_izq = document.getElementsByClassName("cuadro-inf-izq")[0];
+const cuadro_inf_der = document.getElementsByClassName("cuadro-inf-der")[0];
 
+console.log(cuadro_sup_izq);
 
-let ids_aleatorios = [];
+let secuencia_de_botones = [];
 let respaldo_ids_aleatorios = [];
-let puede_seleccionar = false;
+let puede_clickear_cuadros = false;
+let numero_del_cuadro = {
+  0: "cuadro-sup-izq",
+  1: "cuadro-sup-der",
+  2: "cuadro-inf-izq",
+  3: "cuadro-inf-der",
+};
 
 // let divCantidadDRepeticiones = document.getElementById("div_cantidad_D_Combinaciones");
 
 let botonRepetir = document.getElementById("repetir");
 
-
 // let svg_moneda = document.createElement("img");
 // svg_moneda.src = './moneda.svg';
 
 // let div_monedas = document.getElementById("div_monedas");
-
 
 // div_monedas.appendChild(svg_moneda);
 // div_monedas.appendChild(svg_moneda.cloneNode());
@@ -28,324 +32,204 @@ let botonRepetir = document.getElementById("repetir");
 // div_monedas.appendChild(svg_moneda.cloneNode());
 // div_monedas.appendChild(svg_moneda.cloneNode());
 
-
-
-
-
-
-boton_uno.addEventListener('click',
-    function (evento) {
-
-        if (puede_seleccionar === true) {
-
-
-            if (evento.isTrusted) {
-                logicaJuego(0);
-
-            }
-
-
-
-        }
-
-
+cuadro_sup_izq.addEventListener("click", function (evento) {
+  if (puede_clickear_cuadros === true) {
+    if (evento.isTrusted) {
+      evaluar_clicks(0);
     }
-)
+  }
+});
 
-boton_dos.addEventListener('click',
-    function (evento) {
-        if (puede_seleccionar === true) {
-
-            
-            if (evento.isTrusted) {
-                logicaJuego(1);
-
-            }
-
-
-
-        }
-
-
+cuadro_sup_der.addEventListener("click", function (evento) {
+  if (puede_clickear_cuadros === true) {
+    if (evento.isTrusted) {
+      evaluar_clicks(1);
     }
-)
+  }
+});
 
-boton_tres.addEventListener('click',
-    function (evento) {
-        if (puede_seleccionar === true) {
-
-
-            if (evento.isTrusted) {
-                logicaJuego(2);
-
-            }
-
-
-
-        }
-
-
+cuadro_inf_izq.addEventListener("click", function (evento) {
+  if (puede_clickear_cuadros === true) {
+    if (evento.isTrusted) {
+      evaluar_clicks(2);
     }
-)
+  }
+});
 
-boton_cuatro.addEventListener('click',
-    function (evento) {
-        if (puede_seleccionar === true) {
-
-
-            if (evento.isTrusted) {
-                logicaJuego(3);
-
-            }
-
-
-
-        }
-
-
+cuadro_inf_der.addEventListener("click", function (evento) {
+  if (puede_clickear_cuadros === true) {
+    if (evento.isTrusted) {
+      evaluar_clicks(3);
     }
-)
+  }
+});
 
+function correr_animaciones(clase, nombre_d_la_animacion) {
+  switch (nombre_d_la_animacion) {
+    case "clickBotonJugar":
+      anime({
+        targets: clase,
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        opacity: [
+          { value: 1, duration: 200, easing: "easeOutExpo" },
+          { value: 0.2, duration: 300 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
+    case "botonJugarAlIniciar":
+      anime({
+        targets: clase,
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        opacity: [
+          { value: 0.2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
 
+    case "click_cuadro":
+      anime({
+        targets: clase,
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
+    case "acerto":
+      anime({
+        targets: clase,
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
+    case "fallo":
+      anime({
+        targets: clase,
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
+    case "oscurecerBotones":
+      anime({
+        targets: [".bg-uno", ".bg-dos", ".bg-tres", ".bg-cuatro"],
+        opacity: [
+          { value: 1, duration: 200, easing: "easeOutExpo" },
+          { value: 0.2, duration: 900 },
+        ],
+        easing: "easeInOutSine",
+      });
 
+      break;
+    case "escalarBotones":
+      anime({
+        targets: [".bg-uno", ".bg-dos", ".bg-tres", ".bg-cuatro"],
+        scale: [
+          { value: 2, duration: 100, easing: "easeOutExpo" },
+          { value: 1, duration: 500 },
+        ],
+        easing: "easeInOutSine",
+      });
 
-function animacion_botones(clase,elemento) {
-    switch (elemento) {
-        case "clickBotonJugar":
-            anime({
-                targets: clase,
-                scale: [
-                    { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                    { value: 1, duration: 500 }],
-                opacity: [
-                    { value: 1, duration: 200,  easing: 'easeOutExpo' },
-                    { value: 0.2, duration: 300 }],
-                easing: 'easeInOutSine'
-            });
-            break;
-        case "botonJugarAlIniciar":
-            anime({
-                targets: clase,
-                scale: [
-                    { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                    { value: 1, duration: 500 }],
-                opacity: [
-                    { value: 0.2, duration: 100,  easing: 'easeOutExpo' },
-                    { value: 1, duration: 500 }],
-                easing: 'easeInOutSine'
-            });
-            break;
-    
-            case "botones":
-                anime({
-                    targets: clase,
-                    scale: [
-                        { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 1, duration: 500 }],
-                    opacity: [
-                        { value: 1, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 0.2, duration: 500 }],
-                    easing: 'easeInOutSine'
-                });
-            break;
-            case "acerto":
-                anime({
-                    targets: clase,
-                    scale: [
-                        { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 1, duration: 500 }],
-                    opacity: [
-                        { value: 1, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 0.2, duration: 500 }],
-                    easing: 'easeInOutSine'
-                });
-            break;
-            case "fallo":
-                anime({
-                    targets: clase,
-                    scale: [
-                        { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 1, duration: 500 }],
-                    easing: 'easeInOutSine'
-                });
-            break;
-            case "oscurecerBotones":
-                anime({
-                    targets: [".bg-uno",".bg-dos",".bg-tres",".bg-cuatro"],
-                    opacity: [
-                        { value: 1, duration: 200,  easing: 'easeOutExpo' },
-                        { value: 0.2, duration: 900 }],
-                    easing: 'easeInOutSine'
-                });
-
-            break;
-            case "escalarBotones":
-                anime({
-                    targets: [".bg-uno",".bg-dos",".bg-tres",".bg-cuatro"],
-                    scale: [
-                        { value: 2, duration: 100,  easing: 'easeOutExpo' },
-                        { value: 1, duration: 500 }],
-                    easing: 'easeInOutSine'
-                });
-
-            break;
-            case "iluminar":
-                anime({
-                    targets: [".bg-uno",".bg-dos",".bg-tres",".bg-cuatro"],
-                    opacity: [
-                        { value: 1, duration: 200,  easing: 'easeOutExpo' },
-                        { value: 0.2, duration: 900 }],
-                    easing: 'easeInOutSine'
-                });
-            break;
-        default:
-            break;
-    }
-
-
-
+      break;
+    case "iluminar":
+      anime({
+        targets: [".bg-uno", ".bg-dos", ".bg-tres", ".bg-cuatro"],
+        opacity: [
+          { value: 1, duration: 200, easing: "easeOutExpo" },
+          { value: 0.2, duration: 900 },
+        ],
+        easing: "easeInOutSine",
+      });
+      break;
+    default:
+      break;
+  }
 }
 
-
-function animacionInicial() {
-
-    animacion_botones('.btn-info', "botonJugarAlIniciar");
-
+async function evaluar_clicks(numero) {
+  if (numero === secuencia_de_botones[0]) {
+    correr_animaciones(`.${numero_del_cuadro[numero]}`, "acerto");
+    secuencia_de_botones.shift();
+  }
+  if (secuencia_de_botones.length == 0) {
+    puede_clickear_cuadros = false;
+    await siguienteCombinacion();
+    return;
+  }
 }
 
-document.addEventListener("DOMContentLoaded", (event) => { animacionInicial() });
-
-
-
-async function logicaJuego(idBoton) {
-
-    if (ids_aleatorios[0] === idBoton) {
-
-        animacion_botones(".bg-"+ids_botones[idBoton],"acerto");
-        ids_aleatorios.shift();
-        // await click_maquina(idBoton,"acerto");
-        
-        // await animacion_click(idBoton,true);
-        // if (ids_aleatorios.length === 0 && cantidad_pulsaciones <= maximo_nivel) {
-        //     cantidad_pulsaciones++;
-        //     setTimeout(() => {
-        //         click_maquina(cantidad_pulsaciones);
-        //     }, 800);
-        // }
-
-
-        if (ids_aleatorios.length == 0) {
-            puede_seleccionar = false;
-
-            await pausaSiguienteCombinacion();
-            siguienteCombinacion();
-            return;
-            
-        }
-
-
-    } else {
-
-        animacion_botones(".bg-"+ids_botones[idBoton],"fallo");
-        div_monedas.lastChild.remove();
-
-
-    }
-
-}
-
-function pausaSiguienteCombinacion() {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            animacion_botones(null,"escalarBotones");
-            resolve();
-        },500)
-    })
-}
 
 async function repetir() {
-    ids_aleatorios = respaldo_ids_aleatorios.map(_=>_);
-    for (let index = 0; index < ids_aleatorios.length; index++) {
-
-        await click_maquina(ids_aleatorios[index],"botones")
-
-    }
+  secuencia_de_botones = respaldo_ids_aleatorios.map((_) => _);
+  for (let index = 0; index < secuencia_de_botones.length; index++) {
+    await mostrar_secuencia(secuencia_de_botones[index], "botones");
+  }
 }
 
-
-
-
-
-const myModal = new bootstrap.Modal(document.getElementById('myModal'), {})
+// const myModal = new bootstrap.Modal(document.getElementById("myModal"), {});
 
 async function siguienteCombinacion() {
-    numero_instrucciones++;
-    // divCantidadDRepeticiones.innerText = numero_instrucciones;
-    ids_random(numero_instrucciones);
-    for (let index = 0; index < ids_aleatorios.length; index++) {
+  longitud_secuencia++;
+  await pausa(2000);
+  armar_secuencia(longitud_secuencia);
+  for (let index = 0; index < secuencia_de_botones.length; index++) {
+    await mostrar_secuencia(secuencia_de_botones[index], "click_cuadro");
+  }
 
-        await click_maquina(ids_aleatorios[index],"botones")
-
-    }
-
-    puede_seleccionar = true;
+  puede_clickear_cuadros = true;
 }
-
 
 async function iniciarJuego() {
+  secuencia_de_botones = [];
+  $("#btn-inicio").attr("disabled", true);
+  $("#repetir").removeClass("disabled");
+  armar_secuencia(longitud_secuencia);
+  //   await pausaInicio();
 
-    ids_aleatorios = [];
-    $('#inicio').attr('disabled', true);
-    $('#repetir').removeClass('disabled');
-    animacion_botones('.btn-info', "clickBotonJugar");
-    ids_random(numero_instrucciones);
-    // divCantidadDRepeticiones.innerText = numero_instrucciones;
-    await pausaInicio();
+  for (let index = 0; index < secuencia_de_botones.length; index++) {
+    await mostrar_secuencia(secuencia_de_botones[index], "click_cuadro");
+  }
 
-    for (let index = 0; index < ids_aleatorios.length; index++) {
-
-        await click_maquina(ids_aleatorios[index],"botones")
-
-    }
-
-
-    puede_seleccionar = true;
-
+  puede_clickear_cuadros = true;
 }
 
-
-function pausaInicio() {
-    animacion_botones(0,"oscurecerBotones");
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            
-            resolve();
-        },1000)
-    })
+function pausa(milisegundos) {
+  //   correr_animaciones(0, "oscurecerBotones");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, milisegundos);
+  });
 }
 
-function ids_random(cantidad) {
-    while (cantidad > 0) {
-        let indice = Math.floor(4 * Math.random());
-        ids_aleatorios.push(indice);
-        cantidad--;
-    }
-
-    respaldo_ids_aleatorios = ids_aleatorios.map(_=>_);
-
-
+function armar_secuencia(cantidad) {
+  for (let index = 0; index < cantidad; index++) {
+    secuencia_de_botones.push(Math.floor(4 * Math.random()));
+  }
+  respaldo_ids_aleatorios = [...secuencia_de_botones];
 }
 
-
-
-const click_maquina = function (index,elemento) {
-    
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            animacion_botones(".bg-"+ids_botones[index],elemento);
-            resolve();
-        }, 800);
-    })
-
-}
-
+const mostrar_secuencia = function (index, nombre_d_la_animacion) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      correr_animaciones(`.${numero_del_cuadro[index]}`, nombre_d_la_animacion);
+      resolve();
+    }, 800);
+  });
+};
